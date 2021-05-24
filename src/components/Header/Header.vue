387b4ae714e1736a17e023ffe522ce9d31b71e38
Jonathan Akwetey <okinejonathan@yahoo.com>
2021-05-24 17:41:43 +0100

    switch dark and light mode

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,22 +2,48 @@
   <div class="header">
     <ul class="header__navigation">
       <li class="header__navigation-item">
-        <router-link to="/" class="header__navigation-item-link">Home</router-link>
+        <router-link to="/" class="header__navigation-item-link"
+          >Home</router-link
+        >
       </li>
       <li class="header__navigation-item">
-        <router-link to="/meals" class="header__navigation-item-link">Carbonara Meals Calculator</router-link>
+        <router-link to="/meals" class="header__navigation-item-link"
+          >Carbonara Meals Calculator</router-link
+        >
       </li>
       <li class="header__navigation-item">
-        <router-link to="/restaurants" class="header__navigation-item-link">Our Restaurants</router-link>
+        <router-link to="/restaurants" class="header__navigation-item-link"
+          >Our Restaurants</router-link
+        >
       </li>
+      <button @click="toggleTheme" aria-label="Toggle themes">
+        <span v-if="this.theme == 'darkMode'"> Light</span>
+        <span v-else> Dark</span>
+      </button>
     </ul>
   </div>
 </template>
 <style lang="scss">
-@import './Header.scss';
+@import "./Header.scss";
 </style>
 <script>
 export default {
-  name: 'Header'
-}
+  name: "Header",
+  data() {
+    return {
+      theme: "",
+    };
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme == "darkMode" ? "" : "darkMode";
+      document.body.setAttribute("data-theme", this.theme);
+      localStorage.setItem("theme", this.theme);
+    },
+  },
+  mounted() {
+    let localTheme = localStorage.getItem("theme");
+    document.body.setAttribute("data-theme", localTheme);
+  },
+};
 </script>
